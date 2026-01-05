f3e8e923d349a0db8c9fff59d8505292740e68ef
CapJunkrat <zy171157@163.com>
2026-01-05 18:23:22 +0800

    release(tool): Release version 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.johnz.diceroller"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 10
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
